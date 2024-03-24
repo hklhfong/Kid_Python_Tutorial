92c84aa0f7b8db14623682e5bfc9899ca1c8d4f7
Ho Fong Law <hklhfong75@gmail.com>
2024-03-23 20:08:47 -0400

    add profile list

--- Playground/dwitter/admin.py
+++ Playground/dwitter/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-
+from .models import Profile
 
 from django.contrib.auth.models import User, Group
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     # Only display the "username" field
     fields = ["username"]
+    inlines = [ProfileInline]
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
-# Register your models here.
